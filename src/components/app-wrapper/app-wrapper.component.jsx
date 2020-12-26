920e01adb4ac78914bbebfa32701922fd5004782
Olha Halat <olha.halat@gmail.com>
2020-12-27 00:17:28 +0100

    improve styling, add theme switch

--- src/components/app-wrapper/app-wrapper.component.jsx
+++ src/components/app-wrapper/app-wrapper.component.jsx
@@ -1,9 +1,26 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import Switch from '@material-ui/core/Switch';
+
 // 'children' refers here to child elements which are nested inside this one. it is specified which elements are nested in the App class
 export function AppWrapper({ children }) {
+	const [darkState, setDarkState] = useState(false);
+	const paletteType = darkState ? 'dark' : 'light';
+	const darkTheme = createMuiTheme({
+		palette: {
+			type: paletteType,
+		}
+	});
+	const handleThemeChange = () => {
+		setDarkState(!darkState);
+	};
 
 	return (
-		<section className="Wrapper">
-			{ children }
-		</section>
+		<ThemeProvider theme={darkTheme}>
+				<Switch checked={darkState} onChange={handleThemeChange} />
+			<section className="Wrapper">
+				{ children }
+			</section>
+		</ThemeProvider>
 	)
 }
